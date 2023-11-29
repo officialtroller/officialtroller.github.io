33052c857128c1a5ba2c4c90a414437fd79e6000
officialtroller <127138313+officialtroller@users.noreply.github.com>
2023-11-29 20:14:47 +0100

    Add files via upload

--- ecp-generator/js/surv.user.js
+++ ecp-generator/js/surv.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Troll Client
-// @version      0.1.6
+// @version      0.1.7
 // @description  Troll Client
 // @author       official_troller
 // @license      GPL-3.0
@@ -10,7 +10,7 @@
 // @updateURL    https://officialtroller.github.io/ecp-generator/js/surv.user.js
 // @downloadURL  https://officialtroller.github.io/ecp-generator/js/surv.user.js
 // ==/UserScript==
-const CURRENT_RUNNING_VERSION = "0.1.6";
+const CURRENT_RUNNING_VERSION = "0.1.7";
 const log = (msg) => console.log(`%c[Troll Client] ${msg}`, "color: #ffff00");
 const modlog = (msg) => console.log(`%c[Mod] ${msg}`, "color: #FF00A6");
 const stylelog = (msg) => console.log(`%c[Style] ${msg}`, "color: #06c26d");
@@ -263,7 +263,7 @@ function injectLoader() {
                     stylelog("Style loaded successfully");
                     log(`Client loaded successfully`);
                 });
-            }, 30);
+            }, 100);
         }
     };
     xhr.send();
