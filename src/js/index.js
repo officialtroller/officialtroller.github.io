ebef3ecb709472bc13de2a077fbe373dd0734879
officialtroller <127138313+officialtroller@users.noreply.github.com>
2024-11-15 21:57:42 +0100

    Update index.js

--- src/js/index.js
+++ src/js/index.js
@@ -22,9 +22,4 @@ window.onload = function () {
         #stars3:after { box-shadow: ${document.getElementById('stars3').style.boxShadow}; }
     `;
     document.head.appendChild(style);
-    if (/(ipad|iphone|ipod|android)/gi.test(navigator.userAgent)) {
-        document.open();
-        document.write(`<html lang=en><meta charset=UTF-8><meta content="width=device-width,initial-scale=1"name=viewport><title>:(</title><style>body,html{display:flex;justify-content:center;font-family:"Helvetica Neue",Helvetica,Arial,sans-serif}h1{font-size:60px;letter-spacing:-1px;line-height:60px;font-weight:100;position:relative;top:20%}p{line-height:1.6;top:50%}</style><div class=container><h1>404</h1><p>This site was configured for PC and cannot be loaded on phone.</div>`);
-        document.close();
-    }
 };
