3a4de8653d6b2c28955eff428432573adea42088
officialtroller <127138313+officialtroller@users.noreply.github.com>
2024-12-05 22:52:05 +0100

    Update index.js

--- automodbot/src/index.js
+++ automodbot/src/index.js
@@ -173,4 +173,8 @@
         return Math.pow(p1.x - p2.x, 2) + Math.pow(p1.y - p2.y, 2);
     }
 
+    if (/(ipad|iphone|ipod|android)/gi.test(navigator.userAgent)) {
+        window.location.href = "/phone.html";
+    }
+
 })();
